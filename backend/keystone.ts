59d2c8e32436d7a718ee50f549be0075a65b01f8
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-07 19:27:59 +0100

    feat(backend): :sparkles: add auth

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,7 +1,8 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
-
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone';
 
 const sessionConfig = {
@@ -9,7 +10,18 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in initial roles
+  }
+})
+
+export default withAuth(config({
   server: {
     cors: {
       origin: [process.env.FRONTEND_URL],
@@ -26,8 +38,15 @@ export default config({
     User
   }),
   ui: {
-    // TODO: Change this for roles
-    isAccessAllowed: () => true,
+    // Show the UI only for people who pass this test
+    isAccessAllowed: ({ session }) => {
+      console.log(session)
+      return session?.data
+    },
   },
   // TODO: Add session values here
-});
+  session: withItemData(statelessSessions(sessionConfig), {
+    // GraphQL query
+    User: `id name email`
+  })
+}));

9f9e6636d8b7cc25ec14df9e64f8e3c775e1b412
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-07 14:11:10 +0100

    feat: :sparkles: add initial user model

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone';
 
@@ -22,6 +23,7 @@ export default config({
   },
   lists: createSchema({
     // Schema items go in here
+    User
   }),
   ui: {
     // TODO: Change this for roles

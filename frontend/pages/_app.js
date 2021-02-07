8990cdfe9111ddaaddd0dce0eeec1ed7d85df054
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-07 21:05:43 +0100

    feat(frontend): setup Apollo client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,39 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+import propTypes from 'prop-types';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+MyApp.propTypes = {
+  Component: propTypes.any,
+  pageProps: propTypes.any,
+  apollo: propTypes.any,
+};
+
+export default withData(MyApp);
